f0783e4db139cfdb64a8738b457e8c88db29db86
Suhaib <suhaib92khan@gmail.com>
2020-03-03 10:49:45 -0500

    Have near full functioning carousel

--- client/components/splash/popular_stables_carousel.js
+++ client/components/splash/popular_stables_carousel.js
@@ -7,6 +7,15 @@ class PopularCarousel extends Component {
     super(props)
   }
 
+  translateX(event) {
+    const ul = document.getElementById("carousel-ul");
+    if (event.target.className === "splash-carousel-right-arrow") {
+      ul.style.transform = 'translateX(-100%)';
+    } else {
+      ul.style.transform = 'translateX(0%)';
+    }
+  }
+
   render() {
     return (
       <div className="splash-popular-full-container">
@@ -17,17 +26,82 @@ class PopularCarousel extends Component {
           </div>
           <div className="splash-popular-divider"></div>
           <div className="carousel-container">
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+            <ul id="carousel-ul">
+              <li id="first-li">
+                <img src="https://images.otstatic.com/prod/25898919/1/medium.jpg"></img>
+                <div className="carousel-li-bottom-holder">
+                  <h2>Soby's</h2>
+                  <h3>Ratings placeholder</h3>
+                  <p>Booked x times today</p>
+                </div>
+              </li>
+              <li>
+                <img src="https://images.otstatic.com/prod/25898919/1/medium.jpg"></img>
+                <div className="carousel-li-bottom-holder">
+                  <h2>Soby's</h2>
+                  <h3>Ratings placeholder</h3>
+                  <p>Booked x times today</p>
+                </div>
+              </li>
+              <li>
+                <img src="https://images.otstatic.com/prod/25898919/1/medium.jpg"></img>
+                <div className="carousel-li-bottom-holder">
+                  <h2>Soby's</h2>
+                  <h3>Ratings placeholder</h3>
+                  <p>Booked x times today</p>
+                </div>
+              </li>
+              <li>
+                <img src="https://images.otstatic.com/prod/25898919/1/medium.jpg"></img>
+                <div className="carousel-li-bottom-holder">
+                  <h2>Soby's</h2>
+                  <h3>Ratings placeholder</h3>
+                  <p>Booked x times today</p>
+                </div>
+              </li>
+              <li id="first-li">
+                <img src="https://images.otstatic.com/prod/25898919/1/medium.jpg"></img>
+                <div className="carousel-li-bottom-holder">
+                  <h2>Soby's2</h2>
+                  <h3>Ratings placeholder</h3>
+                  <p>Booked x times today</p>
+                </div>
+              </li>
+              <li>
+                <img src="https://images.otstatic.com/prod/25898919/1/medium.jpg"></img>
+                <div className="carousel-li-bottom-holder">
+                  <h2>Soby's2</h2>
+                  <h3>Ratings placeholder</h3>
+                  <p>Booked x times today</p>
+                </div>
+              </li>
+              <li>
+                <img src="https://images.otstatic.com/prod/25898919/1/medium.jpg"></img>
+                <div className="carousel-li-bottom-holder">
+                  <h2>Soby's2</h2>
+                  <h3>Ratings placeholder</h3>
+                  <p>Booked x times today</p>
+                </div>
+              </li>
+              <li>
+                <img src="https://images.otstatic.com/prod/25898919/1/medium.jpg"></img>
+                <div className="carousel-li-bottom-holder">
+                  <h2>Soby's2</h2>
+                  <h3>Ratings placeholder</h3>
+                  <p>Booked x times today</p>
+                </div>
+              </li>
             </ul>
+          </div>
+          <div className="splash-carousel-arrow-container">
+            <div className="splash-carousel-right-arrow" onClick={this.translateX}></div>
+          </div>
+          <div className="splash-carousel-arrow-container2">
+            <div className="splash-carousel-right-arrow2" onClick={this.translateX}></div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
