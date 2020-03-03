c29de7a941011e3738cf994427002e1ca38d4cd1
Suhaib <suhaib92khan@gmail.com>
2020-03-03 14:03:04 -0500

    Add functional and responsive carousel

--- client/components/splash/popular_stables_carousel.js
+++ client/components/splash/popular_stables_carousel.js
@@ -5,27 +5,119 @@ import splashCSS from "./splash.css";
 class PopularCarousel extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      leftArrowDisplay: 'none',
+      rightArrowDisplay: 'shown'
+    }
+
+    this.handleScroll = this.handleScroll.bind(this);
+    this.updateDimensions = this.updateDimensions.bind(this);
   }
 
   translateX(event) {
     const container = document.getElementById("carousel-container");
-    if (event.target.className === "fa fa-angle-right") {
+    if (event.target.className === "splash-carousel-right-arrow" || event.target.id === "right>") {
       // container.style.transform = 'translateX(-100%)';
       container.scrollLeft = 1010
-    } else {
+    } else if (event.target.className === "fa-angle-left" || "splash-carousel-right-arrow2") {
       // container.style.transform = 'translateX(0%)';
       container.scrollLeft = -1010
     }
   }
 
-  // handleScroll() {
-  //   var sdegree = 0;
-  //   sdegree++;
-  //   var srotate = "translateX(" + sdegree + "%)";
-  //   const ul = document.getElementById("carousel-ul");
-  //   ul.style.transform = srotate;
+  updateDimensions() {
+    console.log(window.innerWidth)
 
-  // }
+    const container = document.getElementById("carousel-container");
+    const rightArrow = document.getElementById("right-arr")
+    const leftArrow = document.getElementById("left-arr")
+    
+    if (window.innerWidth > 765 && window.innerWidth < 1618) {
+      if (container.scrollLeft >= 1010) {
+        rightArrow.style.display = 'none'
+      } else {
+        rightArrow.style.display = 'flex';
+        rightArrow.style.justifyContent = 'center';
+      }
+
+      if (container.scrollLeft === 0) {
+        leftArrow.style.display = 'none'
+      } else {
+        leftArrow.style.display = 'flex';
+        leftArrow.style.justifyContent = 'center';
+      }
+    } else if (window.innerWidth > 1618) {
+      if (this.state.rightArrowDisplay === 'none') {
+        rightArrow.style.display = 'none'
+      } else {
+        rightArrow.style.display = 'flex';
+        rightArrow.style.justifyContent = 'center';
+      }
+
+      if (container.scrollLeft === 0) {
+        leftArrow.style.display = 'none'
+      } else {
+        leftArrow.style.display = 'flex';
+        leftArrow.style.justifyContent = 'center';
+      }
+    } else {
+      rightArrow.style.display = 'none'
+      leftArrow.style.display = 'none'
+    }
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions.bind(this));
+  }
+
+
+  handleScroll() {
+    const container = document.getElementById("carousel-container");
+    const rightArrow = document.getElementById("right-arr")
+    const leftArrow = document.getElementById("left-arr")
+    if (window.innerWidth > 765 && window.innerWidth < 1618) {
+      if (container.scrollLeft >= 1010) {
+        rightArrow.style.display = 'none'
+        this.setState({ rightArrowDisplay: 'none' })
+      } else {
+        rightArrow.style.display = 'flex';
+        rightArrow.style.justifyContent = 'center';
+        this.setState({ rightArrowDisplay: 'shown' })
+      }
+
+      if (container.scrollLeft === 0) {
+        leftArrow.style.display = 'none'
+        this.setState({ leftArrowDisplay: 'none' })
+      } else {
+        leftArrow.style.display = 'flex';
+        leftArrow.style.justifyContent = 'center';
+        this.setState({ leftArrowDisplay: 'shown' })
+      }
+    } else if (window.innerWidth > 1618) {
+      if (container.scrollLeft >= 760) {
+        rightArrow.style.display = 'none'
+        this.setState({ rightArrowDisplay: 'none' })
+      } else {
+        rightArrow.style.display = 'flex';
+        rightArrow.style.justifyContent = 'center';
+        this.setState({ rightArrowDisplay: 'shown' })
+      }
+
+      if (container.scrollLeft === 0) {
+        leftArrow.style.display = 'none'
+        this.setState({ leftArrowDisplay: 'none' })
+      } else {
+        leftArrow.style.display = 'flex';
+        leftArrow.style.justifyContent = 'center';
+        this.setState({ leftArrowDisplay: 'shown' })
+      }
+    } else {
+      rightArrow.style.display = 'none'
+      leftArrow.style.display = 'none'
+      this.setState({ rightArrowDisplay: 'none', leftArrowDisplay: 'none' })
+    }
+  }
 
   render() {
     return (
@@ -36,7 +128,7 @@ class PopularCarousel extends Component {
             <a href="#">View all</a>
           </div>
           <div className="splash-popular-divider"></div>
-          <div id="carousel-container" className="carousel-container" >
+          <div id="carousel-container" className="carousel-container" onScroll={this.handleScroll}>
             <ul id="carousel-ul">
               <li id="first-li">
                 <img src="https://images.otstatic.com/prod/25898919/1/medium.jpg"></img>
@@ -105,10 +197,10 @@ class PopularCarousel extends Component {
             </ul>
           </div>
           <div className="splash-carousel-arrow-container">
-            <div className="splash-carousel-right-arrow" onClick={this.translateX}><i class="fa fa-angle-right"></i></div>
+            <div id="right-arr" className="splash-carousel-right-arrow" onClick={this.translateX}><i id="right>" className="fa fa-angle-right"></i></div>
           </div>
           <div className="splash-carousel-arrow-container2">
-            <div className="splash-carousel-right-arrow2" onClick={this.translateX}><i class="fa fa-angle-left"></i></div>
+            <div id="left-arr" className="splash-carousel-right-arrow2" onClick={this.translateX}><i className="fa fa-angle-left"></i></div>
           </div>
         </div>
       </div>
