378b5c761e6e5b1e98b4a67567805d035f20f267
tombetthauser <tombetthauser@gmail.com>
2020-03-03 16:19:24 -0800

    Correct file path for credentials in root folder

--- server/schema/s3.js
+++ server/schema/s3.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
 if (process.env.NODE_ENV !== "production") {
-  AWS.config.loadFromPath("./credentials.json");
+  AWS.config.loadFromPath("../../credentials.json");
 }
 const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
