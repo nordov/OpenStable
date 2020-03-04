0da3a7113e70d6cd7d5eb7383a12e7bb499bf069
tombetthauser <tombetthauser@gmail.com>
2020-03-03 16:08:53 -0800

    Add AWS image query to stable_type.

--- server/schema/types/stable_type.js
+++ server/schema/types/stable_type.js
@@ -7,6 +7,9 @@ const {
   GraphQLList
 } = graphql;
 
+// Tom - This is for AWS.
+const { s3 } = require('../s3');
+
 const mongoose = require("mongoose");
 const Stable = mongoose.model("stables");
 
@@ -39,15 +42,33 @@ const StableType = new GraphQLObjectType({
             .catch(err => null)
       }
     }, 
-    images: { 
-      type: new GraphQLList(require("./image_type")),
-      resolve(parentValue){
-          return Stable.findById(parentValue.id)
-            .populate("images")
-            .then(stable => stable.images)
-            .catch(err => null)
+
+
+    // Tom - Boilerplate, should be adapted to multiple images once tested.
+    image: {
+      type: GraphQLString,
+      resolve(parentValue) {
+        let imageUrl;
+        if (parentValue.image) {
+          imageUrl = s3.getSignedUrl('getObject', {
+            Bucket: "aws-graphql-dev-testing",
+            Key: parentValue.image
+          });
+        }
+        return imageUrl || parentValue.image;
       }
-    },        
+    }
+
+    // Tom - Old image code, just in case something breaks.
+    // images: {
+    //   type: new GraphQLList(require("./image_type")),
+    //   resolve(parentValue){
+    //       return Stable.findById(parentValue.id)
+    //         .populate("images")
+    //         .then(stable => stable.images)
+    //         .catch(err => null)
+    //   }
+    // },        
   })
 });
 
