ccfcde160a1d9a39dc6bdc986e0761ce32037751
tombetthauser <tombetthauser@gmail.com>
2020-03-09 08:02:29 -0700

    Change keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./keys_prod");
-} else {
-  module.exports = require("./keys_dev");
-}
+module.exports = {
+  mongoURI:
+    "mongodb+srv://slack:YIzBxuMTeFSK5sxS@cluster0-wbklx.mongodb.net/openstable?retryWrites=true&w=majority",
+  secretOrKey: 'd383c39d-d96c-4e26-9ddc-80191a010ce4',
+  mapAPI: 'AIzaSyDDbQNUilJ7K7newSnldu811RIlKFAFfrw'
+};
