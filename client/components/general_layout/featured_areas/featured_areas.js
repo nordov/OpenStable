51ca83b79d1a3646b0083297f16b25c8464f5f06
tombetthauser <tombetthauser@gmail.com>
2020-03-08 22:46:51 -0700

    Restyle splash page city links

--- client/components/general_layout/featured_areas/featured_areas.js
+++ client/components/general_layout/featured_areas/featured_areas.js
@@ -21,21 +21,21 @@ class FeaturedAreas extends Component {
               <div className="featured-li-ele featured-li-ele-first">
                 <li id="featured-first-img">
                   <img src="/static/images/newyork.jpeg"></img>
-                  <Link className="overlay" to="/bayarea" to="/bayarea">
+                  <Link className="overlay city-picture-text-link" to="/bayarea" to="/bayarea">
                     <h3>Marin Headlands</h3>
                     <p>stables</p>
                   </Link>
                 </li>
                 <li>
                   <img src="/static/images/chicago.jpeg"></img>
-                  <Link className="overlay" to="/bayarea">
+                  <Link className="overlay city-picture-text-link" to="/bayarea">
                     <h3>Golden Gate Park</h3>
                     <p>stables</p>
                   </Link>
                 </li>
                 <li id="featured-last-img">
                   <img src="/static/images/losangeles.jpeg"></img>
-                  <Link className="overlay" to="/bayarea">
+                  <Link className="overlay city-picture-text-link" to="/bayarea">
                     <h3>Richmond / San Pablo</h3>
                     <p>stables</p>
                   </Link>
@@ -44,21 +44,21 @@ class FeaturedAreas extends Component {
               <div className="featured-li-ele featured-li-ele-second">
                 <li id="featured-first-img">
                   <img src="/static/images/sanfrancisco.jpeg"></img>
-                  <Link className="overlay" to="/bayarea">
+                  <Link className="overlay city-picture-text-link" to="/bayarea">
                     <h3>Oakland Hills / Tilden</h3>
                     <p>stables</p>
                   </Link>
                 </li>
                 <li>
                   <img src="/static/images/miami.jpeg"></img>
-                  <Link className="overlay" to="/bayarea">
+                  <Link className="overlay city-picture-text-link" to="/bayarea">
                     <h3>Alemeda / Hayward</h3>
                     <p>stables</p>
                   </Link>
                 </li>
                 <li id="featured-last-img">
                   <img src="/static/images/lasvegas.jpeg"></img>
-                  <Link className="overlay" to="/bayarea">
+                  <Link className="overlay city-picture-text-link" to="/bayarea">
                     <h3>South San Francisco</h3>
                     <p>stables</p>
                   </Link>
@@ -68,20 +68,20 @@ class FeaturedAreas extends Component {
           </div>
           <div className="splash-featured-text-container">
             <div>
-              <Link to="/bayarea"><p>Marin Headlands</p></Link>
-              <Link to="/bayarea"><p>Mill Valley / San Rafael</p></Link>
+              <Link className="city-text-link" to="/bayarea"><p>Marin Headlands</p></Link>
+              <Link className="city-text-link" to="/bayarea"><p>Mill Valley / San Rafael</p></Link>
             </div>
             <div>
-              <Link to="/bayarea"><p>Richmond / San Pablo</p></Link>
-              <Link to="/bayarea"><p>Oakland Hills / Tilden</p></Link>
+              <Link className="city-text-link" to="/bayarea"><p>Richmond / San Pablo</p></Link>
+              <Link className="city-text-link" to="/bayarea"><p>Oakland Hills / Tilden</p></Link>
             </div>
             <div>
-              <Link to="/bayarea"><p>Alemeda / Hayward</p></Link>
-              <Link to="/bayarea"><p>South San Francisco</p></Link>
+              <Link className="city-text-link" to="/bayarea"><p>Alemeda / Hayward</p></Link>
+              <Link className="city-text-link" to="/bayarea"><p>South San Francisco</p></Link>
             </div>
             <div>
-              <Link to="/bayarea"><p>Daly City / Merced</p></Link>
-              <Link to="/bayarea"><p>Golden Gate Park</p></Link>
+              <Link className="city-text-link" to="/bayarea"><p>Daly City / Merced</p></Link>
+              <Link className="city-text-link" to="/bayarea"><p>Golden Gate Park</p></Link>
             </div>
           </div>
         </div>
