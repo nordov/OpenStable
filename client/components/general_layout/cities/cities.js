7b3ee52f3b83513cb3a68c10983c1368245eb186
tombetthauser <tombetthauser@gmail.com>
2020-03-08 20:33:19 -0700

    Add basic react render to cities

--- client/components/general_layout/cities/cities.js
+++ client/components/general_layout/cities/cities.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './cities.css';
 
 class Cities extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>Cities</h1>
+      </div>
+    )
+  }
 }
 
 export default Cities;
