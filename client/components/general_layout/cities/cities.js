6edf84dbc92592656399da5e5be51f788bb853ce
tombetthauser <tombetthauser@gmail.com>
2020-04-02 14:07:46 -0700

    More city page styling

--- client/components/general_layout/cities/cities.js
+++ client/components/general_layout/cities/cities.js
@@ -27,7 +27,38 @@ class Cities extends React.Component {
             return (
               <div className="search-full-body-container">
                 <div className="search-right-side-container">
-                  <SearchListItem stables={data.stables} />
+                  {/* <SearchListItem stables={data.stables} /> */}
+                  <ul>
+                    {
+                      data.stables.map(stable => (
+                        <li>
+                          <img src={stable.image}></img>
+                          <div className="search-li-container">
+                            <a href="#">{stable.name}</a>
+                            <div className="search-ratings-container">
+                              <i className="fas fa-star"></i>
+                              <i className="fas fa-star"></i>
+                              <i className="fas fa-star"></i>
+                              <i className="fas fa-star"></i>
+                              <i className="fas fa-star"></i>
+                              <p>Awesome</p>
+                            </div>
+                            <div className="search-li-info-container">
+                              <span>$$</span>
+                              <span>Neighborhood</span>
+                            </div>
+                            <div className="search-li-timeslots-container">
+                              <a href="#/book">6:30 PM</a>
+                              <a href="#/book">6:45 PM</a>
+                              <a href="#/book">7:00 PM</a>
+                              <a href="#/book">7:15 PM</a>
+                              <a href="#/book">7:30 PM</a>
+                            </div>
+                          </div>
+                        </li>
+                      ))
+                    }
+                  </ul>
                 </div>
               </div>
             );
