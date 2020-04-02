14a74f2726511c67bb9081167ed035081e7f842e
tombetthauser <tombetthauser@gmail.com>
2020-04-02 13:13:45 -0700

    Get cities search list item working

--- client/components/general_layout/cities/cities.js
+++ client/components/general_layout/cities/cities.js
@@ -2,15 +2,37 @@ import React from 'react';
 import './cities.css';
 import BayAreaCarousel from "../bayarea_stables/bayarea_stables_carousel";
 
+import { Query } from 'react-apollo';
+import Queries from '../../../graphql/queries'
+import SearchListItem from '../../search/search_list_item';
+
+
+
+
 class Cities extends React.Component {
   constructor(props) {
     super(props)
   }
 
   render() {
+    const { FETCH_STABLES } = Queries;
+    
     return (
       <div>
         <BayAreaCarousel />
+        <Query query={FETCH_STABLES}>          
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: `${error}`</p>;
+            return (
+              <div className="search-full-body-container">
+                <div className="search-right-side-container">
+                  <SearchListItem stables={data.stables} />
+                </div>
+              </div>
+            );
+          }}
+        </Query>
       </div>
     )
   }
